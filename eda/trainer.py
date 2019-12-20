fd435b261528d52797a14ab1d7546e133479c3e4
s_shimazu <sho_shimazu@earlycross.co.uk>
2019-12-20 15:59:06 +0900

    add log method in Trainer class

--- eda/trainer.py
+++ eda/trainer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+from optimizer import MIMIC, AffEDA, ECGA
+
 
 class Trainer(object):
     def __init__(self, objective, optim, train_steps=1e5, logger=None):
@@ -27,32 +29,35 @@ class Trainer(object):
 
             gloss = np.min(general_losses)
             # logging
-            if self.logger is not None:
+            self.log(c, general_losses, gloss, info, trial, step)
+
+            if gloss <= self.objective.target:
                 self.logger.info("{} steps, gloss: {:.3f}, theta_convergence: {:.4f}".format(
                                  step,
                                  gloss,
                                  self.optim.theta.max(axis=1).mean()),
                                  step)
-                self.logger.write("general_loss", gloss, trial, step)
-                self.logger.write("best_discrete_vec", torch.argmax(torch.Tensor(c), dim=2)[np.argmin(general_losses)], trial, step)
-                # print("clustering", " - ".join(map(str, [c.idx_set for c in self.optim.cluster])))
-                # self.logger.write("clustering", " - ".join(map(str, [c.idx_set for c in self.optim.cluster])), trial, step)
-                # self.logger.write("eps", self.optim.eps, trial, step)
-                # self.logger.write("delta", self.optim.delta, trial, step)
-                # self.logger.save("theta", self.optim.theta, trial, step)
-                # self.logger.write("start", info["start"], trial, step)
-                # self.logger.write("end", info["end"], trial, step)
-                # self.logger.write("gamma_value", self.optim.gamma, trial, step)
-                # self.logger.save("s_value", self.optim.s, trial, step)
-                # self.logger.save("weight", self.objective.x.detach().numpy(), trial, step)
-
-            if gloss <= self.objective.target:
-                if self.logger is not None:
-                    self.logger.info("{} steps, gloss: {:.3f}, theta_convergence: {:.4f}".format(
-                                     step,
-                                     gloss,
-                                     self.optim.theta.max(axis=1).mean()),
-                                     step)
                 break
             step += 1
         return gloss <= self.objective.target, step
+
+    def log(self, c, general_losses, gloss, info, trial, step):
+        if self.logger is None:
+            return
+        self.logger.info("{} steps, gloss: {:.3f}, theta_convergence: {:.4f}".format(
+                         step,
+                         gloss,
+                         self.optim.theta.max(axis=1).mean()),
+                         step)
+        self.logger.write("general_loss", gloss, trial, step)
+        self.logger.write("best_discrete_vec", torch.argmax(torch.Tensor(c), dim=2)[np.argmin(general_losses)], trial, step)
+        self.logger.save("theta", self.optim.theta, trial, step)
+
+        if isinstance(self.optim, MIMIC):
+            self.logger.write("order", self.optim.order, trial, step)
+        if isinstance(self.optim, ECGA) or isinstance(self.optim, AffEDA):
+            self.logger.write("clustering", " - ".join(map(str, [c.idx_set for c in self.optim.cluster])), trial, step)
+        if "start" in info:
+            self.logger.write("start", info["start"], trial, step)
+        if "end" in info:
+            self.logger.write("end", info["end"], trial, step)
