a904d8b767ffe5f36ed399803043db3f8426430a
e5120 <e5120@gm.kochi-ct.jp>
2020-04-20 12:44:25 +0900

    fix a bug in Tournament selection

--- eda/optimizer/selection/tournament.py
+++ eda/optimizer/selection/tournament.py
@@ -5,6 +5,9 @@ from optimizer.selection.selection_base import SelectionBase
 
 class Tournament(SelectionBase):
     def __init__(self, k=2, sampling_rate=1.0, replace=True):
+        assert 0.0 < sampling_rate <= 1.0
+        if not replace:
+            assert 0.0 < sampling_rate <= 0.5
         self.k = k
         self.sampling_rate = sampling_rate
         self.replace = replace
@@ -12,14 +15,14 @@ class Tournament(SelectionBase):
     def apply(self, population, fitness, sort=False):
         assert self.k <= population.shape[0]
         lam = population.shape[0]
-        target_idx = np.arange(int(self.sampling_rate * lam))
+        sample_lam = int(self.sampling_rate * lam)
         # get population for Tournament
         if self.replace:
             sample_idx = np.array(
-                [np.random.choice(target_idx, self.k, replace=False) for _ in range(lam)]
+                [np.random.choice(lam, self.k, replace=False) for _ in range(sample_lam)]
             )
         else:
-            sample_idx = np.random.choice(target_idx, (int(population.shape[0] / self.k), self.k), replace=False)
+            sample_idx = np.random.choice(lam, (sample_lam, self.k), replace=False)
         # get winner index of each tournament
         winner_idx = np.argmin(fitness[sample_idx], axis=1)
         winner_idx = [sample_idx[i, winner_idx[i]] for i in range(len(winner_idx))]
@@ -28,6 +31,7 @@ class Tournament(SelectionBase):
         fitness = fitness[winner_idx]
         # if True, sort by fitness
         population, fitness = self.sort_by_fitness(population, fitness, sort=sort)
+        print(population.shape)
         return population, fitness
 
     def __str__(self):
