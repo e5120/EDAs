117e03f4513fbc012e0fc9f398c9ef3b61257a08
e5120 <e5120@gm.kochi-ct.jp>
2021-02-18 20:18:54 +0900

    remove unnecessary files

--- eda/objective/util/__init__.py
+++
@@ -1,1 +0,0 @@
-from objective.util.item import Item
--- eda/objective/util/item.py
+++
@@ -1,8 +0,0 @@
-class Item(object):
-    def __init__(self, n, v, w):
-        self.name = n
-        self.value = v
-        self.weight = w
-
-    def __str__(self):
-        return "[{}] weigth = {},\tvalue = {}".format(self.name, self.weight, self.value)
