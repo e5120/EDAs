b7eb7f7c7117b2e9b03dc215bae1c71813de6a15
e5120 <e5120@gm.kochi-ct.jp>
2021-02-19 15:20:34 +0900

    update arguments

--- eda/opts.py
+++ eda/opts.py
@@ -1,56 +1,100 @@
 def objective_opts(parser):
     parser.add_argument("--objective-type", type=str, required=True,
-                        choices=["knapsack", "one_max", "four_peaks",
-                                 "deceptive_order_3", "deceptive_order_4"])
-    parser.add_argument("--setting-path", type=str, required=True)
+                        choices=["one_max",  "two_min", "four_peaks",
+                                 "deceptive_trap", "nk_landscape", "w_model"],
+                        help="specify a objective function.")
+    parser.add_argument("--dim", type=int, required=True,
+                        help="dimension of the objective function.")
+    parser.add_argument("--minimize", action="store_false",
+                        help="the problem is whether minimization problem or maximization problem.")
+    parser.add_argument("-t", type=int, default=3,
+                        help="a user parameter which determines the difficulty of the four-peaks function.")
+    parser.add_argument("-k", type=int, default=3,
+                        help="a user parameter, which determines the ruggedness of the fitness landscape, of deceptive-k trap function and NK-landscape.")
+    parser.add_argument("-d", type=float, default=0.1,
+                        help="a user parameter, which determines the deceptiveness of the fitness landscape, of deceptive-k trap function.")
+    parser.add_argument("--nk-seed", type=int, default=1,
+                        help="a random number seed for NK-landscape where the seed is used to generate k bits and a lookup table randomly.")
+    parser.add_argument("--mu", type=int, default=2,
+                        help="a user parameter, which determines neutrality, of w-model function.")
+    parser.add_argument("-v", type=int, default=4,
+                        help="a user parameter, which determines epistasis, of w-model function.")
+    parser.add_argument("-m", type=int, default=2,
+                        help="a user parameter, which determines mullti-objectivity, of w-model function.")
+    parser.add_argument("-n", type=int, default=5,
+                        help="a user parameter, which determines the dimension of each objective function, of w-model function.")
+    parser.add_argument("--gamma", type=int, default=0,
+                        help="a user parameter, which determines the ruggedness and deceptiveness of the fitness landscape, of w-model function.")
 
 
 def optimizer_opts(parser):
-    parser.add_argument("--optim-type", type=str, default="pbil",
+    parser.add_argument("--optim-type", type=str, required=True,
                         choices=["umda", "pbil", "mimic",
-                                 "ga", "cga", "ecga", "aff_eda"])
-    parser.add_argument("--lam", type=int, default=64)
-    parser.add_argument("--lr", type=float, default=0.1)
-    parser.add_argument("--negative-lr", type=float, default=None)
-    parser.add_argument("--replace-rate", type=float, default=0.1)
+                                 "cga", "ecga", "aff_eda", "boa"],
+                        help="specify a optimizer.")
+    parser.add_argument("--lam", type=int, default=100,
+                        help="population size.")
+    parser.add_argument("--lr", type=float, default=0.1,
+                        help="learning rate in PBIL and UMDA.")
+    parser.add_argument("--negative-lr", type=float, default=None,
+                        help="learning rate for negative example in PBIL.")
     parser.add_argument("--selection", type=str, default="tournament",
-                        choices=["none", "block", "tournament", "roulette", "top"])
-    parser.add_argument("--selection-rate", type=float, default=0.5)
-    parser.add_argument("--sampling-rate", type=float, default=0.5)
-    parser.add_argument("--tournament-size", type=int, default=2)
-    parser.add_argument("--with-replacement", action="store_true")
-    parser.add_argument("--crossover", type=str, default="uniform",
-                        choices=["none", "uniform", "two_point"])
-    parser.add_argument("--crossover-prob", type=float, default=0.8)
-    parser.add_argument("--mutation", type=str, default="none",
-                        choices=["mutation", "none"])
-    parser.add_argument("--mutation-prob", type=float, default=0.01)
-    parser.add_argument("--mutation-shift", type=float, default=0.05)
+                        choices=["none", "block", "tournament", "roulette", "top"],
+                        help="selection method which chooses individuals from a population based on the evaluation value.")
+    parser.add_argument("--selection-rate", type=float, default=0.5,
+                        help="selection rate, i.e., how many individuals are chosen when the selection method is applied to a population.")
+    parser.add_argument("--tournament-size", type=int, default=2,
+                        help="tournament size in the tournament selection.")
+    parser.add_argument("--with-replacement", action="store_true",
+                        help="sampling with replacement or not")
+    parser.add_argument("--mutation-prob", type=float, default=0.01,
+                        help="mutation probability in PBIL.")
+    parser.add_argument("--mutation-shift", type=float, default=0.05,
+                        help="amount of shift for mutation in PBIL.")
     parser.add_argument("--replacement", type=str, default="restricted",
-                        choices=["restricted", "trunc"])
-    parser.add_argument("--window-size", type=int, default=2)
-    parser.add_argument("--train-steps", type=int, default=1e5)
+                        choices=["truncation", "restricted"],
+                        help="replacement method which replaces individuals in parent population with ones in candidate population.")
+    parser.add_argument("--replace-rate", type=float, default=0.1,
+                        help="replacement rate, i.e., how many individuals are replaced when the replacement method is applied to a population.")
+    parser.add_argument("--constraint-k", type=int, default=None,
+                        help="maximum number of parents of each node in BOA.")
+    parser.add_argument("--metric", type=str, default="bic",
+                        choices=["aic", "bic", "k2"],
+                        help="evaluation criterion to measure the degree of dependencies among variables in BOA.")
+    parser.add_argument("--window-size", type=int, default=2,
+                        help="a user parameter, which determines trade-off between the goodness and the diversity in the population, in the restricted tournament replacement.")
+    parser.add_argument("--max-num-evals", type=int, default=1e5,
+                        help="Maximum number of evaluations.")
 
 
 def utils_opts(parser):
-    parser.add_argument("--seed", type=int, default=-1)
-    parser.add_argument("--seeds", type=str, nargs="+", default=None)
-    parser.add_argument("--trials", type=int, default=1)
+    parser.add_argument("--seed", type=int, default=-1,
+                        help="a random number seed for trials.")
+    parser.add_argument("--seeds", type=str, nargs="+", default=None,
+                        help="a random number seed for each trials. the length of the seeds must match the number of trials.")
+    parser.add_argument("--trials", type=int, default=1,
+                        help="how many independent trials.")
 
 
 def log_opts(parser):
-    parser.add_argument("--log-dir", type=str, default=None)
-    parser.add_argument("--logging-step", type=int, default=1)
-    parser.add_argument("--display-step", type=int, default=10)
-    parser.add_argument("--save-param-step", type=int, default=10)
+    parser.add_argument("--log-dir", type=str, default=None,
+                        help="directory path to output logs.")
+    parser.add_argument("--logging-step", type=int, default=1,
+                        help="interval of outputting logs to directory.")
+    parser.add_argument("--display-step", type=int, default=10,
+                        help="interval of displaying logs to stdout.")
+    parser.add_argument("--save-param-step", type=int, default=10,
+                        help="")
 
 
 def plot_opts(parser):
     parser.add_argument("--path", type=str, required=True,
-                        help="対象のログが存在するディレクトリを指定．")
+                        help="directory path where the logs were output.")
     parser.add_argument("--save-path", type=str, default=None,
-                        help="プロット結果を保存するディレクトリ．\
-                        Noneの場合，--pathと同じディレクトリに保存される．")
-    parser.add_argument("--target", type=str, default="general_loss")
-    parser.add_argument("--xaxis", type=str, default="train steps")
-    parser.add_argument("--yaxis", type=str, default=None)
+                        help="directory path where figures are output. If not specified, the directory will be the same as the above path.")
+    parser.add_argument("--target", type=str, default="general_loss",
+                        help="")
+    parser.add_argument("--xaxis", type=str, default="train steps",
+                        help="")
+    parser.add_argument("--yaxis", type=str, default=None,
+                        help="")
