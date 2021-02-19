349708440c1102a08650abb6e1e33145b24330bb
e5120 <e5120@gm.kochi-ct.jp>
2021-02-19 11:17:23 +0900

    update PBIL

--- eda/optimizer/pbil.py
+++ eda/optimizer/pbil.py
@@ -1,57 +1,56 @@
 import numpy as np
 
-from optimizer.eda_base import EDABase
+from eda.optimizer.eda_base import EDABase
 
 
 class PBIL(EDABase):
+    """
+    A class of population-based incremental learning (PBIL)
+    """
     def __init__(self, categories, lr, lam=64, negative_lr=None,
                  mut_prob=0.02, mut_shift=0.05, theta_init=None):
         super(PBIL, self).__init__(categories, lam=lam, theta_init=theta_init)
+        """
+        Parameters
+        ----------
+        lr : float
+            Learning rate.
+        negative_lr : float, default None
+            Learning rate for negative example.
+            if None, disable the use of negative examples.
+        mut_prob : float, default 0.02
+            Mutation probability.
+        mut_shift : float, default 0.05
+            Amount of shift in mutation.
+        """
         # only binary strings
         assert self.Cmax == 2
         assert 0.0 < lr < 1.0
         assert negative_lr is None or 0.0 < negative_lr < 1.0
         assert 0.0 <= mut_prob <= 1.0
-        
         self.lr = lr
         self.negative_lr = negative_lr
         self.mut_prob = mut_prob
         self.mut_shift = mut_shift
 
-    def update(self, c_one, fxc, range_restriction=False):
-        self.eval_count += c_one.shape[0]
-        # sort by fitness
-        idx = np.argsort(fxc)
-        best_idx = idx[0]
-        # store best individual and evaluation value
-        if self.best_eval > fxc[best_idx]:
-            self.best_eval = fxc[best_idx]
-            self.best_indiv = c_one[best_idx]
+    def update(self, x, evals, range_restriction=False):
+        x, evals = self._preprocess(x, evals)
         # update probability vector
-        best_indiv = c_one[best_idx]
+        best_indiv = x[0]
         self.theta[:, -1] = (1.0 - self.lr) * self.theta[:, -1] + self.lr * best_indiv[:, -1]
-        # update probability vector by negative sample
+        # update probability vector by negative example
         if self.negative_lr is not None:
-            worst_idx = idx[-1]
-            worst_indiv = c_one[worst_idx]
+            worst_indiv = x[-1]
             diff_dix = best_indiv[:, -1] != worst_indiv[:, -1]
             self.theta[diff_dix, -1] = (1.0 - self.negative_lr) * self.theta[diff_dix, -1] + self.negative_lr * best_indiv[diff_dix, -1]
         # mutation probability vector
-        dim = self.theta.shape[0]
-        mut_idx = np.random.rand(dim) < self.mut_prob
+        mut_idx = np.random.rand(self.d) < self.mut_prob
         mut_num = np.sum(mut_idx)
         self.theta[mut_idx, -1] = (1.0 - self.mut_shift) * self.theta[mut_idx, -1] \
                                   + np.random.randint(0, 2, mut_num) * self.mut_shift
         self.theta[:, 0] = 1 - self.theta[:, -1]
         # if range_restriction is True, clipping
-        for i in range(self.d):
-            ci = self.C[i]
-            theta_min = 1.0 / (self.valid_d * (ci - 1)) if range_restriction and ci > 1 else 0.0
-            self.theta[i, :ci] = np.maximum(self.theta[i, :ci], theta_min)
-            theta_sum = self.theta[i, :ci].sum()
-            tmp = theta_sum - theta_min * ci
-            self.theta[i, :ci] -= (theta_sum - 1.0) * (self.theta[i, :ci] - theta_min) / tmp
-            self.theta[i, :ci] /= self.theta[i, :ci].sum()
+        self.clipping(range_restriction)
 
     def __str__(self):
         sup_str = "    " + super(PBIL, self).__str__().replace("\n", "\n    ")
--- scripts/pbil.sh
+++ scripts/pbil.sh
@@ -1,19 +1,19 @@
 . var
 
-# [one_max|four_peaks|deceptive_order_3|deceptive_order_4|knapsack]
-target=one_max
-python ${src_dir}/main.py \
-        --objective-type ${target} \
-        --setting-path ${root_dir}/settings/${target}.ini \
+# [one_max|two_min|four_peaks|deceptive_trap|nk_landscape|w_model]
+objective=one_max
+python ${root_dir}/main.py \
+        --objective-type ${objective} \
+        --dim 100 \
         --optim-type pbil \
-        --lam 100 \
+        --lam 32 \
         --lr 0.1 \
         --negative-lr 0.075 \
         --mutation-prob 0.02 \
         --mutation-shift 0.05 \
-        --train-steps 2000 \
-        --trials 10 \
+        --max-num-evals 10000 \
+        --trials 3 \
+        --seed -1 \
         --logging-step 1 \
         --display-step 1 \
-        --save-param-step 100000000
-        # --log-dir result/${target}
+        # --log-dir log/pbil_${objective}
