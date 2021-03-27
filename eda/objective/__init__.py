1f6d3117a01685cdbcd16265dd8c947f5b58287a
shimazu <e5120@gm.kochi-ct.jp>
2021-03-27 14:43:28 +0900

    remove objective functions

--- eda/objective/__init__.py
+++
@@ -1,7 +0,0 @@
-from eda.objective.objective_base import ObjectiveBase
-from eda.objective.one_max import OneMax
-from eda.objective.two_min import TwoMin
-from eda.objective.four_peaks import FourPeaks
-from eda.objective.deceptive_trap import DeceptiveTrap
-from eda.objective.nk_landscape import NKLandscape
-from eda.objective.w_model import WModel
